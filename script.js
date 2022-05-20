407ca787e1e7cc1401d436da4c843ae44000bab3
IHaveTheSourceCode <marcinbureta@gmail.com>
2022-05-20 23:45:53 +0200

    add buttons

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// function that makes grid and appends it into container
 const container = document.querySelector(".grid-container");
 // defines base grid size
 let grid_size = 16;
 
+// function that makes grid and appends it into container
 function setGrid(grid_size) {
   container.style.display = "grid";
   container.style.gridTemplateColumns = "repeat(" + grid_size + ", 1fr)";
@@ -22,7 +22,8 @@ function setGrid(grid_size) {
 // adds event to every grid cells that fills it with block
 // when holding mousedown
 function fillCells(div) {
-  div.style.backgroundImage = "url(/images/blocks-flat/yellow_wool.png)";
+  div.style.backgroundImage =
+    "url(/images/blocks-flat/" + chosen_block + ".png";
   div.style.backgroundSize = "cover";
 }
 
@@ -48,31 +49,73 @@ slider.oninput = function () {
   grid_size = slider.value;
   resetGrid();
   setGrid(grid_size);
-  fillCells();
 };
+
 // adds event to every 3D block which returns
 // .tooltip-text textContent on click
-const images = document.querySelectorAll(".tooltip-image");
-const block_names = document.querySelectorAll(".tooltiptext");
-
-function allocate(images, block_name) {
-  for (let i = 0; i < images.length; i++) {
-    images[i].addEventListener("click", () => {
-      return block_names[i].textContent;
-    });
-  }
-}
-allocate(images, block_names);
-
-// gets 2D block by name of 3D block
+const tooltips = Array.from(document.querySelectorAll(".tooltip"));
 let chosen_block = "";
 
-function get_block() {}
+function get_block() {
+  tooltips.forEach((tooltip) =>
+    tooltip.addEventListener("click", function () {
+      chosen_block = tooltip.textContent.trim();
+      console.log(tooltip.textContent.trim());
+    })
+  );
+}
+get_block();
 
-// changes opacity to 1 and img:active to some background color
+//turns the grid lines on/off
+const lines_btn = document.querySelector(".lines-btn");
+lines_btn.addEventListener("click", function () {
+  if (lines_btn.textContent == "Hide lines") {
+    document
+      .querySelectorAll(".img-div")
+      .forEach((div) => (div.style.border = "none"));
+    lines_btn.textContent = "Show lines";
+    lines_btn.style.backgroundColor = "gray";
+    document.querySelector(".grid-container").style.boxShadow = "none";
+  } else {
+    lines_btn.textContent = "Hide lines";
+    lines_btn.style.backgroundColor = "#9ca09c";
+    document
+      .querySelectorAll(".img-div")
+      .forEach(
+        (div) =>
+          (div.style.cssText =
+            "border-left: 1px solid black; border-top: 1px solid black")
+      );
+    document.querySelector(".grid-container").style.boxShadow =
+      "1px 1px 0px black";
+  }
+});
 
-// changes opacity to 1 and img:active to some background colro
-
-//turns the lines on/off
+// changes background of the page
+const background_switcher = document.querySelector(".toggle-background");
+let background = "light";
+background_switcher.addEventListener("click", function () {
+  if (background == "light") {
+    background = "dark";
+    background_switcher.textContent = "Dark mode";
+    document.body.style.backgroundImage = "url(/images/themes/theme-dark.jpg)";
+    document.querySelector(".header-title").style.color = "white";
+    document.querySelector(".grid-container").style.boxShadow =
+      "1px 1px 0px white";
+    document
+      .querySelectorAll(".img-div")
+      .forEach((image) => (image.style.borderColor = " white"));
+  } else {
+    background = "light";
+    background_switcher.textContent = "Light mode";
+    document.body.style.backgroundImage = "url(/images/themes/theme-light.jpg)";
+    document.querySelector(".header-title").style.color = "black";
+    document.querySelector(".grid-container").style.boxShadow =
+      "1px 1px 0px black";
+    document
+      .querySelectorAll(".img-div")
+      .forEach((image) => (image.style.borderColor = " black"));
+  }
+});
 
 // pickaxe tool that will delete blocks
